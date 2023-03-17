38359e708debb42dd5e64f2abe532fcdde2fd2c0
sobhanhadikhah <hadikhahs@gmail.com>
2023-03-17 14:23:45 +0330

    start about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,14 @@ import useTitle from "../hooks/usetitle"
 const About = () => {
     useTitle("About")
     return (
-        <div>about</div>
+        <div className="max-w-[1240px] mx-auto h-screen " >
+            <div>
+                <h1>About Page</h1>
+                <div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
